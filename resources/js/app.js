5057b3027187eb541407ac0bf76e301ded356cac
mario.arista <mario.arista.dev@gmail.com>
2024-04-12 14:58:57 +0200

    Js bootstrap + section/yield

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import './bootstrap';
 // Import our custom CSS
 import '~resources/scss/app.scss';
 
+import '~bootstrap/js/index.esm.js';
+
 /* Import Bootstrap 5 */
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts/app')
+
+@section('title')
+    
+@endsection
+
+@section('content')
+    <div class="container py-5">
+        <h1>Ciao!</h1>
+    </div>
+@endsection
